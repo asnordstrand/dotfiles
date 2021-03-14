684100ee9bc7d7a5ea703ffd77de4b7ade5e0158
Anna Nordstrand <anna@nordstrand.dev>
2021-03-14 04:49:06 -0500

    refactor: write coc configuration in lua

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,4 +1,5 @@
 require('plugins')
+require('coc')
 
 local g = vim.g
 local cmd = vim.cmd
@@ -9,12 +10,10 @@ local map = utils.map
 -- Color scheme
 opt('o', 'termguicolors', true)
 opt('o', 'background', 'dark')
-cmd 'colorscheme gruvbox8'
+cmd [[colorscheme gruvbox8]]
 
 -- Settings
 local indent = 2
-cmd 'syntax enable'
-cmd 'filetype plugin indent on'
 opt('w', 'number', true)
 opt('w', 'cursorline', true)
 opt('o', 'incsearch', true)
@@ -27,18 +26,3 @@ opt('b', 'shiftwidth', indent)
 opt('b', 'autoindent', true)
 opt('b', 'expandtab', true)
 opt('b', 'smartindent', true)
-
-cmd 'set completeopt=menuone,noinsert,noselect'
-cmd 'set shortmess+=c'
-cmd 'set updatetime=100'
-cmd 'set signcolumn=yes'
-
--- coc.nvim extensions
-g.coc_global_extensions = {
-  'coc-pairs',
-  'coc-rust-analyzer',
-  'coc-css',
-  'coc-html',
-  'coc-json',
-  'coc-tsserver',
-}
